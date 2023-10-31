0fa6db7d8013428ba541f96ca66dffaea2c75c0b
panyada choojuntraporn <Cre4mS>
2023-10-31 12:11:10 +0700

    update

--- insertsuccess.php
+++ insertsuccess.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>เพิ่มข้อมูลแพ็คเกจ</title>
 </head>
 <body>
     <form action="datapackage.php">
